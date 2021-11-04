ff08e177b40f4fcdb670adafc564fac66c6e8449
David Mayfield <davidmayfield1818@yahoo.com>
2021-11-03 17:36:45 -0700

    animations for 2,3 fixed

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -7,6 +7,9 @@ public class AnimationScript : MonoBehaviour
 
     private Animator anim;
     private Movement move;
+    private MovementPolish move2;
+    private MovementDistinct move3;
+    private MoveSelector ms;
     private Collision coll;
     [HideInInspector]
     public SpriteRenderer sr;
@@ -16,7 +19,10 @@ public class AnimationScript : MonoBehaviour
         anim = GetComponent<Animator>();
         coll = GetComponentInParent<Collision>();
         move = GetComponentInParent<Movement>();
+        move2 = GetComponentInParent<MovementPolish>();
+        move3 = GetComponentInParent<MovementDistinct>();
         sr = GetComponent<SpriteRenderer>();
+        ms = GetComponentInParent<MoveSelector>();
     }
 
     void Update()
@@ -24,10 +30,27 @@ public class AnimationScript : MonoBehaviour
         anim.SetBool("onGround", coll.onGround);
         anim.SetBool("onWall", coll.onWall);
         anim.SetBool("onRightWall", coll.onRightWall);
-        anim.SetBool("wallGrab", move.wallGrab);
-        anim.SetBool("wallSlide", move.wallSlide);
-        anim.SetBool("canMove", move.canMove);
-        anim.SetBool("isDashing", move.isDashing);
+        if(ms.moveStyle == 1)
+        {
+            anim.SetBool("wallGrab", move.wallGrab);
+            anim.SetBool("wallSlide", move.wallSlide);
+            anim.SetBool("canMove", move.canMove);
+            anim.SetBool("isDashing", move.isDashing);
+        }
+        else if(ms.moveStyle == 2)
+        {
+            anim.SetBool("wallGrab", move2.wallGrab);
+            anim.SetBool("wallSlide", move2.wallSlide);
+            anim.SetBool("canMove", move2.canMove);
+            anim.SetBool("isDashing", move2.isDashing);
+        }
+        else
+        {
+            anim.SetBool("wallGrab", move3.wallGrab);
+            anim.SetBool("wallSlide", move3.wallSlide);
+            anim.SetBool("canMove", move3.canMove);
+            anim.SetBool("isDashing", move3.isDashing);
+        }
 
     }
 
